1c50cd96a2948dcc609d160e4a3a4c84cf7bff98
ace <381187081@qq.com>
2022-10-29 22:43:30 +0800

    feat: component

---
+++ packages/admin-layout/src/components/LayoutContent.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<Props>(), {
+  paddingTop: 0,
+  paddingBottom: 0,
+  paddingLeft: 0,
+  transitionDuration: 300,
+  transitionTimingFunction: 'ease-in-out',
+})
+defineOptions({ name: 'content' })
+interface Props {
+  /** 顶部内边距 */
+  paddingTop?: number
+  /** 底部内边距 */
+  paddingBottom?: number
+  /** 左侧内边距 */
+  paddingLeft?: number
+  /** 是否禁止溢出 */
+  overflowHidden?: boolean
+  /** 动画过渡时间 */
+  transitionDuration?: number
+  /** 动画过渡时间 */
+  transitionTimingFunction?: string
+}
+
+const d = props.paddingBottom
+</script>
+
+<template>
+  <main :style="style" class="admin-layout__content" />
+</template>
--- packages/admin-layout/vite.config.ts
+++ packages/admin-layout/vite.config.ts
@@ -3,10 +3,21 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import dts from 'vite-plugin-dts'
+import DefineOptions from 'unplugin-vue-define-options/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), DefineOptions(), AutoImport(), dts({
+    include: ['./src/index.ts', './src/index.vue'],
+    beforeWriteFile(filePath, content) {
+      return {
+        filePath: filePath.replace('/dist/src/', '/dist/'),
+        content,
+      }
+    },
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
